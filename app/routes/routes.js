a843cb688fbca52732af51f0e618e1af9190b3cd
Your Name <you@example.com>
2024-10-12 09:34:33 +0700

    update

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -3,26 +3,14 @@ module.exports = app => {
 
   const router = require("express").Router();
 
-  // Create a new Tutorial
   router.get("/", controller.costPerMonth);
-  //
-  // // Retrieve all controller
+
   router.get("/", controller.serviceCostPerMonth);
-  //
-  // Retrieve all published controller
+
   router.get("/guests", controller.guest);
-  //
-  // Retrieve a single Tutorial with id
+
   router.get("/serviceRevenue", controller.serviceRevenue);
-  //
-  // // Update a Tutorial with id
-  // router.put("/:id", controller.update);
-  //
-  // // Delete a Tutorial with id
-  // router.delete("/:id", controller.delete);
-  //
-  // // Create a new Tutorial
-  // router.delete("/", controller.deleteAll);
+
 
   app.use("/api/v1", router);
 };
